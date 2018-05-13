b087b3072d3cfd7cd8a7a1d947fa125edba975e8
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-05-13 21:05:04 +1000

    Exercise: FizzBuzz

--- myscript.js
+++ myscript.js
@@ -226,7 +226,7 @@ var i = '1';
  }
 
  // Better solution no 2.
- 
+
  for (var i = 3; i <= 22; i = i + 1) {
      if (i%3 == 0) {
          console.log(i);
@@ -239,4 +239,41 @@ var i = '1';
 
 for (var i = 0; i < favouriteFood.length; i = i + 1){
      console.log(favouriteFood[i]);
+}
+
+// Exercise: FizzBuzz
+
+/* Count from 1 to 50 and print the numbers out:
+
+If a number is a multiple of three, print ‘Fizz’.
+If it’s a multiple of 5, print ‘Buzz’.
+If it’s a multiple of 3 and 5, print ‘FizzBuzz’.
+For everything else, print the number itself. */
+
+for (var i = 1; i <= 50; i = i + 1){
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log('FizzBuzz');   
+    } else if (i % 5 == 0) {
+        console.log('Buzz');
+    } else if (i % 3 == 0) {
+         // if (i ** 3) {
+        console.log('Fizz');
+    } else {
+        console.log(i);
+    };
+};
+
+for( var i= 0; i < 50; i++){
+    if(i % 15 == 0){
+        console.log("FizzBuzz");
+    }
+    else if(i % 3 == 0){
+        console.log("Fizz")
+    }
+    else if(i % 5 == 0){
+        console.log("Buzz");
+    }
+    else {
+        console.log(i);
+    }
 }
