19a368601e11f10f21ae8a6749383b89edd6e93c
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-05-13 18:13:07 +1000

    create variables and display them

---
+++ myscript.js
@@ -0,0 +1,13 @@
+/* 1. Create two empty variables named animalName and animalNoise
+2. Assign them appropriate values.
+3. Display them each using alert() */
+
+var animalName;
+var animalNoise;
+
+animalName = "Cat";
+animalNOise = "Neoh-ong";
+alert('animalName');
+
+var message = 'Say "Hello" to my little friend.';
+var warning = "Don't swim here.  There are sharks!";
