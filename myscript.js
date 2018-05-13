2de4ea11a5bffc6b7a0960dd76d03c7aa1a6c01e
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-05-13 20:55:57 +1000

    array exercise

--- myscript.js
+++ myscript.js
@@ -123,3 +123,45 @@ if (operator == '/') {
 } else {
     console.log("Sorry, we don't know this operator");
 };
+
+// ✦ Array excercise 
+
+var randomThings = [2, 'book', 'today is Saturday', 10];
+console.log(randomThings.length);
+
+var randomThings = [2, 'book', 'today is Saturday', 10];
+var firstItem = randomThings[0];
+console.log(firstItem)
+var secondItem = randomThings[1];
+console.log(secondItem) 
+
+var animals = ['cat', 'dog', 'horse'];
+// let's replace 'dog' with 'fish'
+animals[1] = 'fish';
+console.log(animals); // Now our animals array will be ['cat', 'fish', 'horse']
+
+var animals = ['cat', 'dog', 'horse'];
+animals.push('rabbit');
+console.log(animals); // animals will be ['cat', 'dog', 'horse', 'rabbit']
+
+/* 1. Create your own array, named favouriteFood, and write in a couple of things you like.
+2. Check how many values you have in your array favouriteFood. Log the result (console.log())
+3. Get third element from your array favouriteFood and print on the console.
+4. Take your array favouriteFood and replace the first value with anything else. Log the whole array to check.(Don’t forget, index positions start from 0!)
+5. Let’s extend your list of favouriteFood and add one more value to it. console.log list your favouriteFood and check it. */
+
+// start in lowercase, can do "favouritefood", but better to distinguish with "favouriteFood" 
+var favouriteFood = ['kimchi stew', 'dimsum', 'bbq'];
+console.log(favouriteFood.length);   
+
+var thirdItem = favouriteFood[2];
+console.log(thirdItem);
+
+console.log(favouriteFood[2]);
+
+favouriteFood[0] = 'gamjatang';
+console.log(favouriteFood);
+
+favouriteFood.push('soy bean stew');
+console.log(favouriteFood)
+
