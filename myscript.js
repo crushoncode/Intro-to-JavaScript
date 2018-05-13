5bec76dab0afb1adbe19489a69c7c802d898c26f
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-05-13 18:16:06 +1000

    include arithmetical operators

--- myscript.js
+++ myscript.js
@@ -11,3 +11,17 @@ alert('animalName');
 
 var message = 'Say "Hello" to my little friend.';
 var warning = "Don't swim here.  There are sharks!";
+
+/* 1. Create 3 variables:
+1st variable named ten with value 10 inside
+2nd variable named three with value 3
+3rd variable named multipleOfNumbers that will be equal to 1st variable multiplied by the 2nd variable.
+2. As a result, display the value of multipleOfNumbers. */
+
+var sumOfNumbers = 1 + 3;
+alert(sumOfNumbers);
+
+var num1 = 10;
+var num2 = 3;
+var multipleOfNumbers = num1 * num2;
+alert(multipleOfNumbers);
